9ad8c06ed75c7597cde0fa88bbeafce6f41fba04
sergiovsg <sergiovsg99@usal.es>
2021-02-09 13:14:36 +0100

    Grid-view Comentarios enlazandolo con tablas de Usuarios, Articulos y Tiendas

--- models/ComentariosSearch.php
+++ models/ComentariosSearch.php
@@ -14,11 +14,17 @@ class ComentariosSearch extends Comentarios
     /**
      * {@inheritdoc}
      */
+
+    public $nomTienda;
+    public $nickCreador;
+    public $nickModif;
+    public $nomArticulo;
+
     public function rules()
     {
         return [
             [['id', 'tienda_id', 'articulo_id', 'valoracion', 'comentario_id', 'cerrado', 'num_denuncias', 'bloqueado', 'crea_usuario_id', 'modi_usuario_id'], 'integer'],
-            [['texto', 'fecha_denuncia1', 'notas_denuncia', 'fecha_bloqueo', 'notas_bloqueo', 'crea_fecha', 'modi_fecha', 'notas_admin'], 'safe'],
+            [['texto', 'fecha_denuncia1', 'notas_denuncia', 'fecha_bloqueo', 'notas_bloqueo', 'crea_fecha', 'modi_fecha', 'notas_admin', 'nomTienda', 'nickCreador', 'nickModif', 'nomArticulo'], 'safe'],
         ];
     }
 
@@ -48,13 +54,41 @@ class ComentariosSearch extends Comentarios
             'query' => $query,
         ]);
 
+
+        //Configurar la ordenacion por el nombre completo.
+        $orden= $dataProvider->sort;//Aprovechar el objeto de ordenacion interno.
+        $orden->attributes['nomTienda']= [
+            'asc' => ['tiendas.nombre_tienda' => SORT_ASC],
+            'desc' => ['tiendas.nombre_tienda' => SORT_DESC],
+            //'default' => SORT_DESC,
+        ];
+        $orden->attributes['nickCreador']= [
+            'asc' => ['usuarios.nick' => SORT_ASC],
+            'desc' => ['usuarios.nick' => SORT_DESC],
+            //'default' => SORT_DESC,
+        ];
+        $orden->attributes['nickModif']= [
+            'asc' => ['usuarios.nick' => SORT_ASC],
+            'desc' => ['usuarios.nick' => SORT_DESC],
+            //'default' => SORT_DESC,
+        ];
+        $orden->attributes['nomArticulo']= [
+            'asc' => ['articulos.nombre' => SORT_ASC],
+            'desc' => ['articulos.nombre' => SORT_DESC],
+            //'default' => SORT_DESC,
+        ];
+
         $this->load($params);
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
             // $query->where('0=1');
+
+            $query->joinWith(['tiendas']);
             return $dataProvider;
         }
+
+        $query->joinWith(['tiendas','usuarios','articulos']);
 
         // grid filtering conditions
         $query->andFilterWhere([
@@ -77,8 +111,11 @@ class ComentariosSearch extends Comentarios
         $query->andFilterWhere(['like', 'texto', $this->texto])
             ->andFilterWhere(['like', 'notas_denuncia', $this->notas_denuncia])
             ->andFilterWhere(['like', 'notas_bloqueo', $this->notas_bloqueo])
-            ->andFilterWhere(['like', 'notas_admin', $this->notas_admin]);
-
+            ->andFilterWhere(['like', 'notas_admin', $this->notas_admin])
+            ->andFilterWhere(['like', 'tiendas.nombre_tienda', $this->nomTienda])
+            ->andFilterWhere(['like', 'usuarios.nick', $this->nickCreador])
+            ->andFilterWhere(['like', 'usuarios.nick', $this->nickModif])
+            ->andFilterWhere(['like', 'articulos.nombre', $this->nomArticulo]);
         return $dataProvider;
     }
 }
