b0f4e11369d3e7c8931663bf5ce9413d0078d9a0
mperleo <tiomakelelle@outlook.com>
2021-02-10 17:04:49 +0100

    Modificado el main para añadir un acceso a las copias de seguridad

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -42,6 +42,7 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             ['label' => 'Categorías', 'url' => ['/categorias']],
+            ['label' => 'Copias de Seguridad', 'url' => ['/copias-seg']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
