7633d9037b51fbddad185c9d83c7fa57e8657a95
sergiovsg <72349354+sergiovsg@users.noreply.github.com>
2021-02-12 12:49:54 +0100

    Add files via upload

--- views/Tiendas/propietarios_view.php
+++ views/Tiendas/propietarios_view.php
@@ -17,8 +17,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Update', ['propietarios_update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['propietarios_delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
