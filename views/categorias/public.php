7377f22f9f0b7b638db62e229c7c2554dd5ab278
JoseLuisrm13 <JoseLuisrm@usal.es>
2021-02-13 18:20:23 +0100

    Añadida vista para usuarios estandar (queda mostrar boton desde el index)

--- views/categorias/public.php
+++ views/categorias/public.php
@@ -24,22 +24,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php  //echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <h2>Nube de categorías</h2>
     <?php recorrerCategorias($categorias,1)?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'nombre',
-            'descripcion:ntext',
-            'icono',
-        ],
-    ]); 
-    ?>
-
-
 </div>
