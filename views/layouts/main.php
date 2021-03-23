6f0eda7b698319c03b36bd061d0ee6cbfca94f92
PieroMC98 <piero11123@hotmail.com>
2021-03-23 12:17:24 +0100

    aggregate Mantenimiento button on layouts/main

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -51,6 +51,8 @@ AppAsset::register($this);
     						'id' => Yii::$app->user->identity->id,
     					],
     					['label' => 'logout', 'url' => '/user/logout'],
+						Yii::$app->user->rool == 'admin' ? 
+    					['label' => 'Mantenimiento', 'url' => '/usuarios'] : [''],
     				],
     			],
     	],
